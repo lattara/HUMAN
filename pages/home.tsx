56bb921181407e38dfcdd3f08662ad7e14e3cea4
lattara <atopic.ie@gmail.com>
2020-12-20 13:51:58 +0100

    setting up home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import Logo from '../components/logo/Logo'
+import styles from '../styles/Home.module.scss'
+import Image from 'next/image'
 function home() {
     return (
-        <div>
+            <div className={`${styles.container} row`}>
+                <div className={`${styles.divider} col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12`}></div>
+                <div className={`${styles.yellowBlock} col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12`}></div>   
+            </div>
             
-        </div>
     )
+    
 }
 
 export default home
