71ddb858fff953532b998c882184ba35adf25493
lattara <atopic.ie@gmail.com>
2021-01-19 12:02:19 +0100

    adding routes

--- pages/home.tsx
+++ pages/home.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 import Logo from '../components/logo/Logo'
 import styles from '../styles/Home.module.scss'
 import { Row, } from 'react-bootstrap';
+import Link from 'next/link';
 
 function home() {
     return (
         <>
             <Row className={styles.textureBlock} >
                 <Logo isMain={true} />
-                <img className={styles.insta} src="/instagram-bw.png" alt=""/>
+               <Link href={"https://www.instagram.com/human_bijoux/"}><img className={styles.insta} src="/instagram-bw.png" alt=""/></Link>
             </Row>
             <Row className={styles.yellowBlock}>
-            <img className={styles.etsy} src="/etsy.png" alt=""/>
+            <Link href={"https://www.etsy.com/fr/shop/HumanBijouxCreations"}><img className={styles.etsy} src="/etsy.png" alt=""/></Link>
             </Row>
         </>
     )
