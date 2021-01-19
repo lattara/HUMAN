bfabe8306e8987a7165844cfc72ab5fee105eb01
lattara <atopic.ie@gmail.com>
2021-01-19 11:56:50 +0100

    links to social media added

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Logo from '../components/logo/Logo'
 import styles from '../styles/Home.module.scss'
-import { Row } from 'react-bootstrap';
+import { Row, } from 'react-bootstrap';
 
 function home() {
     return (
         <>
             <Row className={styles.textureBlock} >
                 <Logo isMain={true} />
+                <img className={styles.insta} src="/instagram-bw.png" alt=""/>
             </Row>
             <Row className={styles.yellowBlock}>
+            <img className={styles.etsy} src="/etsy.png" alt=""/>
             </Row>
         </>
     )
