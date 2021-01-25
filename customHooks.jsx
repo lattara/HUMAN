0a5b63f024dbbbdd7335278db3b1f879b6d68e7e
lattara <atopic.ie@gmail.com>
2021-01-25 20:56:22 +0100

    implementing custom hooks to handle responsive component rendering

---
+++ customHooks.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+export const useViewport = () => {
+    const [width, setWidth] = useState(0);
+  
+    React.useEffect(() => {
+      setWidth(window.innerWidth)
+      const handleWindowResize = () => setWidth(window.innerWidth);
+      window.addEventListener("resize", handleWindowResize);
+      return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
+  
+    return { width };
+  }
+  
