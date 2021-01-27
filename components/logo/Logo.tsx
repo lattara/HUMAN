791a832cfb1493458261c6c4051ebe8b084b29fa
lattara <atopic.ie@gmail.com>
2021-01-27 20:29:30 +0100

    resolved

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import styles from "./Logo.module.scss"
 import Typewriter from 'typewriter-effect';
 import { Row, Col } from 'react-bootstrap';
+import Link from 'next/link';
+import Tooltip from "@material-ui/core/Tooltip";
+
+
 
 function Logo(props) {
 
@@ -17,36 +21,46 @@ function Logo(props) {
                     typewriter.typeString('.')
                         .pauseFor(2500)
                         .start()
-                }} />
+                }}
+            />
+            <Link href={"https://www.instagram.com/human_bijoux/"}>
+                <Tooltip
+                    title={"Lien vers Instagram"}
+                    placement="top">
+                    <img className={styles.insta} src="/instagram-bw.png" alt="" />
+                </Tooltip>
+            </Link>
             <Col xs={12} className={styles.dividerMain}></Col>
-            <Col xs={12} className={styles.secondTitleMain}>Bijoux au pieres natureles</Col>
+            <div>
+                <Col xs={12} className={styles.secondTitleMain}>Bijoux au pieres natureles
+            <Link href={"https://www.etsy.com/fr/shop/HumanBijouxCreations"}>
+                        <Tooltip
+                            title={"Lien vers Etsy"}
+                            placement="bottom">
+                            <img className={styles.etsy} src="/etsy.png" alt=""
+                            />
+                        </ Tooltip>
+                    </Link>
+                </Col>
+            </div>
         </Row>
 
     const renderOtherPage =
         <Col xs={12} sm={12} md={{ span: 3, offset: 1 }} lg={{ span: 3, offset: 1 }} xl={12} className={styles.otherPages}>
             <Row noGutters className={styles.logoOtherPages}>
-                
                 <Col xs={0} sm={0} md={3} lg={3} xl={2} className={styles.presentationImage}>
                     <img
                         src={props.imageLink}
                         alt="Necklace presentation photo" />
                 </Col>
-                
                 <Col xs={12} sm={12} md={6} lg={8} xl={8}>
                     <div className={styles.mainLogo}>HUMAN</div>
                     <div className={styles.productCategory}>{props.productCategory}</div>
                     <div className={styles.dividerOtherPages}></div>
                     <Col className={styles.undertitle}>{props.undertitle}</Col>
                 </Col>
-                
-                
-                
-                
             </Row>
         </Col>
-
-
-
     return (
         <React.Fragment>
             {props.isMain ? renderMainPage : renderOtherPage}
--- pages/home.tsx
+++ pages/home.tsx
@@ -2,17 +2,16 @@ import React from 'react'
 import Logo from '../components/logo/Logo'
 import styles from '../styles/Home.module.scss'
 import { Row, } from 'react-bootstrap';
-import Link from 'next/link';
+
 
 function home() {
     return (
         <>
             <Row className={styles.textureBlock} >
                 <Logo isMain={true} />
-               <Link href={"https://www.instagram.com/human_bijoux/"}><img className={styles.insta} src="/instagram-bw.png" alt=""/></Link>
+             
             </Row>
             <Row className={styles.yellowBlock}>
-            <Link href={"https://www.etsy.com/fr/shop/HumanBijouxCreations"}><img className={styles.etsy} src="/etsy.png" alt=""/></Link>
             </Row>
         </>
     )
