f24bff8651aa2f11c2efe37e262d057f66ce9714
lattara <atopic.ie@gmail.com>
2021-02-19 15:05:06 +0100

    slider in modal implemented

--- components/modals/ProductModal.tsx
+++ components/modals/ProductModal.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './ProductModal.module.scss'
 import { Modal, Col, Row } from 'react-bootstrap';
 import HumanButton from '../button/Button';
+import ModalSlider from './ModalSlider';
 
 
 function ProductModal(props) {
@@ -16,27 +17,36 @@ function ProductModal(props) {
             <Modal.Header className={styles.header} closeButton>
             </Modal.Header>
             <Modal.Body>
-                <Row>
+                <Row className={styles.modalBody} >
                     <Col lg={6}>
-                   <img src="/presentation1.png" alt=""   />
+                        {/* <img src="/presentation1.png" alt=""   /> */}
+                        <ModalSlider />
                     </Col>
                     <Col lg={4}>
                         <h4>{props.product.name}</h4>
                         <p> {props.product.description}</p>
                         <p>{props.product.price},00€</p>
-                        <Row>{props.product.tags.map(eachTag=> <div key ={props.product.tags.indexOf(eachTag)} className={styles.tag}>{eachTag}</div>)}</Row>
+                        <Row>{props.product.tags.map(tag => <div key={props.product.tags.indexOf(tag)} className={styles.tag}>{tag}</div>)}</Row>
                     </Col>
-                    </Row>
+                </Row>
             </Modal.Body>
             <Modal.Footer className={styles.header}>
-            <HumanButton
-                                style={"full"}
-                                text={"Fermer"}
-                                status="disabled"
-                                buttonImage="magic.png"
-                                tooltip="Voir les détails" 
-                                onClick={props.onHide}
-                                />
+                <HumanButton
+                    style={"full"}
+                    text={"Fermer la fenêtre"}
+                    status="disabled"
+                    buttonImage="magic.png"
+                    tooltip="Voir les détails"
+                    onClick={props.onHide}
+                />
+                <HumanButton
+                    style={"full"}
+                    text={"Ajouter au panier"}
+                    status="disabled"
+                    buttonImage="cart.png"
+                    tooltip="Voir les détails"
+                    onClick={props.onHide}
+                />
             </Modal.Footer>
         </Modal>
     );
