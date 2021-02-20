c93d7b93090d319e9bc7641f6045e44f0f2ab527
lattara <atopic.ie@gmail.com>
2021-02-20 19:56:00 +0100

    radio buttons implemented

--- components/dashboard/DashProductForm.tsx
+++ components/dashboard/DashProductForm.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import { Formik, Field, Form, FormikHelpers, useFormik, FormikProvider } from 'formik';
+import { Formik, Field, Form, FormikHelpers, useFormik, FormikProvider, FieldArray } from 'formik';
 import HumanButton from '../button/Button';
+import ReactChip from 'react-chip'
+import Button from '@material-ui/core/Button';
+import { Row } from 'react-bootstrap';
+import { resourceLimits } from 'worker_threads';
+import { FormControlLabel, Radio, RadioGroup } from '@material-ui/core';
+import { text } from '@fortawesome/fontawesome-svg-core';
 
 const onSubmit = values => { alert("form sent") }
 const initialValues = {
     name: '',
-    email: '',
-    message: '',
+    category: '',
+    tags: [],
+    images: [],
+    price: '',
 }
 
 const validate = values => {
     let errors = {
-        email: '',
         name: '',
         message: ''
     }
@@ -19,11 +26,7 @@ const validate = values => {
     if (!values.name) {
         errors.name = "name is mandatory"
     }
-    if (!values.email) {
-        errors.email = 'emails is mandatory';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address';
-    }
+
     if (!values.message) {
         errors.message = "message is mandatory"
     }
@@ -32,29 +35,86 @@ const validate = values => {
 }
 
 function DashProductForm() {
-   
+
     const formik = useFormik({
         initialValues: initialValues,
         onSubmit: onSubmit,
         validate: validate
     })
 
-    return(
-        <Formik value = { formik } initialValues = { initialValues } onSubmit = { onSubmit } >
-                <form onSubmit={formik.handleSubmit} action="">
-                <label htmlFor="nom">Nom</label>
-                        <Field
-                            id="name"
-                            name="name"
-                            placeholder="Votre nom ici.."
-                            onChange={formik.handleChange}
-                            value={formik.values.name}
-                            formentry
-                        />
-                        {formik.errors.name && formik.touched.name && formik.isValid === false ? <div>{formik.errors.name}</div> : <div></div>}
-                </form>
-    </Formik >
-    
+    const test=(value)=>{console.log(value)}
+
+    return (
+        <Formik value={formik} initialValues={initialValues} onSubmit={onSubmit} >
+            {({ values }) => (
+                <Form onSubmit={formik.handleSubmit}
+                    action="/multiple-upload" method="POST"
+                    encType="multipart/form-data">
+                    <label htmlFor="nom">Nom de produit</label>
+                    <Field
+                        id="name"
+                        name="name"
+                        placeholder="Nom de produit ici ..."
+                        onChange={formik.handleChange}
+                        value={formik.values.name}
+                        formentry
+                    />
+                    {formik.errors.name && formik.touched.name && formik.isValid === false ? <div>{formik.errors.name}</div> : <div></div>}
+
+                    <label htmlFor="category">Catégorie du produit</label>
+                    <div style={{ display: "flex" }}>
+
+                    <input type="radio" name="category" value="necklace" onChange={formik.handleChange} />
+                    <input type="radio" name="category" value="bracelet" onChange={formik.handleChange} />
+                    <input type="radio" name="category" value="earing" onChange={formik.handleChange} />
+
+                    </div>
+
+
+
+                    <label htmlFor="nom">Description de produit</label>
+                    <Field as="textarea"
+                        id="name"
+                        name="name"
+                        placeholder="Votre nom ici.."
+                        onChange={formik.handleChange}
+                        value={formik.values.name}
+                        formentry
+                    />
+                    {formik.errors.name && formik.touched.name && formik.isValid === false ? <div>{formik.errors.name}</div> : <div></div>}
+
+
+                    <label htmlFor="nom">Prix de produit</label>
+                    <Field
+                        id="name"
+                        name="price"
+                        placeholder="Votre nom ici.."
+                        onChange={formik.handleChange}
+                        value={formik.values.name}
+                        formentry
+                    />
+                    {formik.errors.name && formik.touched.name && formik.isValid === false ? <div>{formik.errors.name}</div> : <div></div>}
+
+                    <label htmlFor="nom">Les labels</label>
+                    <Field as="textarea"
+                        id="name"
+                        name="name"
+                        placeholder="Votre nom ici.."
+                        onChange={formik.handleChange}
+                        value={formik.values.name}
+                        formentry
+                    />
+                    {formik.errors.name && formik.touched.name && formik.isValid === false ? <div>{formik.errors.name}</div> : <div></div>}
+
+                    <label htmlFor="nom">Les photos</label>
+                    <input type="file" multiple />
+                    <Row style={{ display: "flex", justifyContent: "flex-end", marginTop: "30px" }}>
+                        <Button type="submit" variant="outlined" color="secondary" onClick={() => { console.log(formik.values) }}>Save</Button>
+                    </Row>
+                </Form>
+            )}
+        </Formik >
+
 
 
     )
--- pages/dashboard/products.tsx
+++ pages/dashboard/products.tsx
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Table from '../../components/table/HumanTable'
-import {dataMOCK} from "../../components/table/data.MOCK"
-import { Row, Col } from 'react-bootstrap'
+import { dataMOCK } from "../../components/table/data.MOCK"
+import { Row, Col, Tab, Tabs } from 'react-bootstrap'
+import DashProductForm from '../../components/dashboard/DashProductForm';
+
 
 function products() {
+    const [key, setKey] = useState('new');
+
+
+
     return (
-        <Row>
-            <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                <h2>Des produits</h2>
-                <dd>Ici tu peux modifier / supprimer ton produits</dd>
-                </Col>
-            <Col>
-            <Table data={dataMOCK} />
-            </Col>
-        </Row>
+
+        <Tabs
+            id="controlled-tab-example"
+            activeKey={key}
+            onSelect={(k) => setKey(k)}
+        >
+            <Tab eventKey="list" title="Liste de produits déjà ajoutée">
+                <Row>
+                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <h2>Des produits</h2>
+                        <dd>Ici tu peux modifier / supprimer ton produits</dd>
+                    </Col>
+                    <Col>
+                        <Table data={dataMOCK} />
+                    </Col>
+                </Row>
+            </Tab>
+            <Tab eventKey="new" title="Ajouter de nouveaux produits">
+            <Row>
+                    <Col xs={12} sm={12} md={10} lg={10} xl={10}>
+                        <h2>Des produits</h2>
+                        <dd>Ici tu peux ajouter un nouveau produit</dd>
+                        <DashProductForm />
+                    </Col>
+                    </Row>
+               
+            </Tab>
+        </Tabs>
+
+
+
     )
 }
 
